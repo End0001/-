4053f6e33f4114c7ba727a8212df65905d8a6543
终 <1263465240@qq.com>
2023-10-19 23:15:26 +0800

    一个简易的验证进制运算代码（用于检查作业）

---
+++ 验证进制运算.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num = -1246;//
+	cout << hex << num << endl;
+
+	return 0;
+
+}
---
+++ 验证进制运算2.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	char t = 'T';
+
+	cout << "ASCII of char t in dec:" << int(t) << endl;
+
+	cout << "ASCII of char t in hex:" << hex << int(t) << endl;
+
+	return 0;
+
+}
