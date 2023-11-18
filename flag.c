5d805996e8958376e2d118f78db7a7e9ee4c84a2
终 <1263465240@qq.com>
2023-11-18 12:26:07 +0800

    打印一个旗帜

---
+++ flag.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main(void)
+{
+	
+	for(int k=1;k<=6;k++)
+	{ 
+	  printf("************************\n");
+	}
+		
+	
+	
+	for (int j = 1; j <= 7; j++)
+	{
+		printf("*\n");
+	}
+	return 0;
+}
