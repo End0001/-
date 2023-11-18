080a36916afef905961dcda53f5ca72f091148a3
终 <1263465240@qq.com>
2023-11-18 12:27:06 +0800

    出租车计费问题
    出租车计价：某城市普通出租车收费标准如下：起步里程为 3 公里，起步费 10 元；超起 步里程后 10 公里内，每公里 2 元；超过 10 公里以上的部分加收 50%的回空补贴费，即每 公里 3 元；营运过程中，因路阻及乘客要求临时停车的，按每 5 分钟 2 元计收(不足 5 分钟 则不收费 )。运价计费尾数四舍五入，保留到元。编写程序，输入行驶里程（公里）与等待 时间（分钟），计算并输出乘客应支付的车费（元）。

--- text.c
+++ text.c
@@ -1,10 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+
 int main()
 {
-	float a = 0;
-	scanf("%f", &a);
-	int b = (int)a;
-	printf("%d", b);
-	return 0;
+	int distance=0;
+	int time=0;
+
+	printf("请输入里程和停车总时间：");
+	scanf("%d %d", &distance, &time);//输入总路程和停车时间
+
+	if (distance <= 0 || time <= 0)
+	{
+		printf("请输入有效值！");//输入值不符时提示用户
+		return 0;
+	}
+
+	int money1=0;
+	if (distance <= 3&&distance>0)
+	{
+		money1 = 10;
+	}
+	else if (distance > 3 && distance <= 13)//运算不同路程的费用
+	{
+		money1 = 10 + (distance - 3) * 2;
+	}
+	else if(distance > 13)
+	{
+		money1 = 30 + (distance - 13) * 3;
+	}
+
+	int money2=0;
+	if (time < 5&& time>0)
+	{
+		money2 = 0;
+	}
+	else if (time >= 5)
+	{
+		money2 = (time / 5)*2 ;//计算不同停车时间的费用
+	}
+
+	int total;
+	total = money1 + money2;//计算总费用
+	printf("应支付车费%d元", total);//输出
+	
 }
